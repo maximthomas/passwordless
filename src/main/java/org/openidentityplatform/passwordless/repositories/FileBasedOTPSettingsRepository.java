9892e2685a7f600419ad44c6df419751a29d63e9
Maxim Thomas <maxim.thomas@gmail.com>
2019-05-16 11:03:15 +0300

    add copyright

--- src/main/java/org/openidentityplatform/passwordless/repositories/FileBasedOTPSettingsRepository.java
+++ src/main/java/org/openidentityplatform/passwordless/repositories/FileBasedOTPSettingsRepository.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.openidentityplatform.passwordless.repositories;
 
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -6,10 +22,8 @@ import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import lombok.extern.log4j.Log4j2;
 import org.apache.commons.lang3.StringUtils;
 import org.openidentityplatform.passwordless.models.OTPSetting;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ResourceLoader;
-import org.springframework.util.ResourceUtils;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
